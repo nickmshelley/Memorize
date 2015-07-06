d4867a04337c66b1dfa58803b6223dcdeb51de58
Nick Shelley <nickmshelley@gmail.com>
2015-07-06 08:21:57 -0600

    Add secondary sort when sorting cards.

--- Memorize/UserDataControllerHelpers.swift
+++ Memorize/UserDataControllerHelpers.swift
@@ -12,7 +12,15 @@ extension UserDataController {
     
     func sampledCardsFromCards(cards: [Card], sampleNumber: Int, isNormalReview: Bool) -> [Card] {
         var sampledCards = [Card]()
-        var partitioned = isNormalReview ? cards.sorted{ $0.normalReviewState.numSuccesses < $1.normalReviewState.numSuccesses }.partitionBy { $0.normalReviewState.numSuccesses } : cards.sorted{ $0.reverseReviewState.numSuccesses < $1.reverseReviewState.numSuccesses }.partitionBy { $0.reverseReviewState.numSuccesses }
+        var partitioned = cards.sorted {
+            let firstReviewState = isNormalReview ? $0.normalReviewState : $0.reverseReviewState
+            let secondReviewState = isNormalReview ? $1.normalReviewState : $1.reverseReviewState
+            if firstReviewState.numSuccesses == secondReviewState.numSuccesses {
+                return firstReviewState.nextReviewDate < secondReviewState.nextReviewDate
+            } else {
+                return firstReviewState.numSuccesses < secondReviewState.numSuccesses
+            }
+            }.partitionBy { isNormalReview ? $0.normalReviewState.numSuccesses : $0.reverseReviewState.numSuccesses }
         while sampledCards.count < sampleNumber {
             for dayArray in partitioned {
                 if sampledCards.count < sampleNumber {
